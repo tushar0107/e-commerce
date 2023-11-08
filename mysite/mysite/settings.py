699d1c4969699f5bcb02cf218e9c11a31db4125c
tushar0107 <tushardasare1701@gmail.com>
2023-11-08 15:12:41 +0530

    production env created

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iw_o+n=88oro)^2qa4c@wr3sj_^h2o24lp5id96gbmpjrk*b8x'
+SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['52.62.22.106', '127.0.0.1']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS', default='52.62.22.106')]
 
 
 # Application definition
@@ -61,8 +62,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin for development
 # In production, you should specify the allowed origins explicitly, e.g.:
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://my-store-seven-xi.vercel.app',
+    config('CORS_ALLOWED_ORIGINS', default='https://my-store-seven-xi.vercel.app')
 ]
 
 
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / config('DATABASE_NAME'),
     }
 }
 
@@ -148,8 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'https://my-store-seven-xi.vercel.app',
+    config('CORS_ALLOWED_ORIGINS', default='https://my-store-seven-xi.vercel.app')
 ]
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',
